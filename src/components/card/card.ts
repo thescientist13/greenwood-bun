a915544cf335ad45605dedb2c0a8eb39227b4eb3
Owen Buckley <owenbuckley13@gmail.com>
2025-10-04 16:59:14 -0400

    constructable stylesheets

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -1,3 +1,5 @@
+import sheet from './card.css' with { type: 'css'};
+
 export default class Card extends HTMLElement {
 
   selectItem() {
@@ -17,49 +19,6 @@ export default class Card extends HTMLElement {
       const template = document.createElement('template');
 
       template.innerHTML = `
-        <style>
-          div {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 0.5rem;
-            border: 1px solid #818181;
-            width: fit-content;
-            border-radius: 10px;
-            padding: 2rem 1rem;
-            height: 680px;
-            justify-content: space-between;
-            background-color: #fff;
-            overflow-x: hidden;
-          }
-          button {
-            background: var(--color-accent);
-            color: var(--color-white);
-            padding: 1rem 2rem;
-            border: 0;
-            font-size: 1rem;
-            border-radius: 5px;
-            cursor: pointer;
-          }
-          img {
-            max-width: 500px;
-            min-width: 500px;
-            width: 100%;
-          }
-          h3 {
-            font-size: 1.85rem;
-          }
-
-          @media(max-width: 768px) {
-            img {
-              max-width: 300px;
-              min-width: 300px;
-            }
-            div {
-              height: 500px;
-            }
-          }
-        </style>
         <div>
           <h3>${title}</h3>
           <img src="${thumbnail}" alt="${title}" loading="lazy" width="100%">
@@ -69,6 +28,8 @@ export default class Card extends HTMLElement {
       this.attachShadow({ mode: 'open' });
       this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
+
+    this.shadowRoot.adoptedStyleSheets = [sheet];
   }
 }
 
